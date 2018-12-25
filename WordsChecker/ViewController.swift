72cbe3d1a46ad57ea8af7064ac137bdb7517d9cb
Kun Huang <kkun.huang@gmail.com>
2018-12-24 21:21:47 -0500

    created UI and connected the outlets and actions

--- WordsChecker/ViewController.swift
+++ WordsChecker/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         testWords()
+    }
+    
+    @IBAction func checkWords(_ sender: UIButton) {
     }
     
     func testWords() {
