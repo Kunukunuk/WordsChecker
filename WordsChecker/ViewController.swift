771eb6cb6b5fe18049fec38a1b953da540b41dee
Kun Huang <kkun.huang@gmail.com>
2018-12-24 21:19:05 -0500

    Initial Commit

---
+++ WordsChecker/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  WordsChecker
+//
+//  Created by Kun Huang on 12/24/18.
+//  Copyright © 2018 Kun Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        testWords()
+    }
+    
+    func testWords() {
+        var correctStr : String = String()
+        let str = "elloh"
+        let textChecker = UITextChecker()
+        
+        let misspelledRange = textChecker.rangeOfMisspelledWord(in: str, range: NSRange(0..<str.utf16.count), startingAt: 0, wrap: false, language: "en_US")
+        if misspelledRange.location != NSNotFound,
+            let guesses = textChecker.guesses(forWordRange: misspelledRange, in: str, language: "en_US") {
+            
+            for each in guesses {
+                print(each)
+            }
+            
+        }
+        
+        //print(correctStr)
+    }
+
+}
+
