0ce3b2f963525511a1e58c7cfebbb8e635c7e7f6
Kun Huang <kkun.huang@gmail.com>
2018-12-25 23:13:48 -0500

    create table cell and connected the cell to table view cell

---
+++ WordsChecker/WordCell.swift
@@ -0,0 +1,24 @@
+//
+//  WordCell.swift
+//  WordsChecker
+//
+//  Created by Kun Huang on 12/25/18.
+//  Copyright © 2018 Kun Huang. All rights reserved.
+//
+
+import UIKit
+
+class WordCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
