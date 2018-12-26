6fc9d096477c8552c2d2d6984dc00b2bc7af420d
Kun Huang <kkun.huang@gmail.com>
2018-12-25 22:54:25 -0500

    removed debug codes

--- WordsChecker/ViewController.swift
+++ WordsChecker/ViewController.swift
@@ -27,51 +27,27 @@ class ViewController: UIViewController {
     func checkPossibleWords(of: String) {
         
         var wordArray = Array(of)
-        //var permutationOfWords = permutations(size: wordArray.count, charArray: wordArray)
         var possibleWords = Array<String>()
         
         permutations(size: wordArray.count, charArray: &wordArray, possibleArray: &possibleWords)
         
         print(possibleWords)
         
-        /*for _ in 0..<wordArray.count {
-            let word = String(wordArray)
-            let returnedWords = testWords(combinedWord: word)
-            let removed = wordArray.removeFirst()
-            wordArray.append(removed)
-            print(word)
-            //numberOfPossibleWord.append(contentsOf: returnedWords)
-        }*/
-        
-        //var finalWords = Set(numberOfPossibleWord)
-        
-        //let filteredWords = finalWords.filter { $0.count <= of.count}
-        
-        //print(permutationOfWords)
-        //print(wordArray)
-        
     }
     
     func permutations(size: Int, charArray: inout Array<Character>, possibleArray: inout Array<String>){
         
-        //var permutationArray = charArray
-        
         if size == 1 {
             let word = String(charArray)
             possibleArray.append(word)
-            print(charArray)
-            //return permutationArray
             return
         }
         
         for index in 0..<size-1 {
             permutations(size: size - 1, charArray: &charArray, possibleArray: &possibleArray)
             charArray.swapAt(size-1, (size%2==1) ? 0:index)
-            //permutationArray = permutations(size: size - 1, charArray: &permutationArray)
-            //permutationArray.swapAt(size - 1, (size%2 == 1) ? 0:index)
         }
         permutations(size: size-1, charArray: &charArray, possibleArray: &possibleArray)
-        //return permutations(size:size - 1, charArray: &permutationArray)
         
     }
     
